8f73cc069d3e1c2a90fb54da022e80407031a7e0
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2020-11-17 13:54:17 -0300

    Firts Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    // Armazenar uma lista de repositórios do gitHub
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(async () => {
+        const response = await fetch('https://api.github.com/users/pedrofaria18/repos');
+        const data = await response.json();
+        setRepositories(data);
+    }, []);
+
+    useEffect(() => {
+        const filtered = repositories.filter(repo => repo.favorite);
+        document.title = `Você tem ${filtered.length} favoritos`;
+    }, [repositories]);
+
+    function handleFarovite(id) {
+        const newRepositories = repositories.map(repo => {
+            return repo.id === id ? {...repo, favorite: !repo.favorite} : repo;
+        });
+
+        setRepositories(newRepositories);
+    }
+
+    return (
+        <ul>
+            { repositories.map(repo => (
+                <li key={repo.id}>
+                    {repo.name}
+                    {repo.favorite && <span>(Favorito)</span>}
+                    <button onClick={() => handleFarovite(repo.id)}>Favoritar</button>
+                </li>
+            ))}
+        </ul>
+    );
 }
-
-export default App;
